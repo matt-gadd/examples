2d19c8dd69750a7470574c8af4b0015e06f858a4
Matthew Gadd <mgadd@sitepen.com>
2016-08-22 12:29:20 +0100

    remove extra-rxjs fix typings, drop has

--- todo-mvc/scripts/flatten-dojo-deps.sh
+++ todo-mvc/scripts/flatten-dojo-deps.sh
@@ -13,6 +13,7 @@ do
 	cp $BAKPKG $PKG
 	echo "fixing package.json: $PKG"
 	sed -i '' 's/"main": "index.js"/"main": "main.js"/g' $PKG
+	sed -i '' "s/\"typings\": \"\.\/dist\/umd\/$D\.d\.ts\"/\"typings\": \"$D.d.ts\"/g" $PKG
 	for F in `find $D -name '*.js' -type f`
 	do
 		echo "fixing imports: $F"
