b12d9adf0338e7f5781602f114f6729763e69909
Matt Gadd <mgadd@sitepen.com>
2016-11-09 19:42:01 +0000

    implement child states inline for now

--- todo-mvc/src/widgets/createTodoList.ts
+++ todo-mvc/src/widgets/createTodoList.ts
@@ -11,6 +11,19 @@ type TodoListOptions = RenderMixinOptions<TodoListState>;
 export type TodoList = RenderMixin<TodoListState>;
 
 const createTodoList = createRenderMixin
+	.mixin({
+		initialize(instance: TodoList, options: any) {
+			instance.on('statechange', ({ state }) => {
+				const { children } = state;
+				if (children.length && options.stateFrom) {
+					const promises = children.map((childId) => options.stateFrom.get(childId));
+					Promise.all(promises).then((childStates) => {
+						state.childStates = childStates;
+					});
+				}
+			});
+		}
+	})
 	.extend({
 		getChildrenNodes(this: TodoList): any[] {
 			const children = this.state.childStates || [];
